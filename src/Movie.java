36cc8bda8b629af53ca2897e58c3698dcb056183
Cornel <140949562+Cornel0210@users.noreply.github.com>
2023-09-01 12:11:34 +0300

    add Movie class

---
+++ src/Movie.java
@@ -0,0 +1,30 @@
+public class Movie {
+    private int _id;
+    private String name;
+    private double rating;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+}
