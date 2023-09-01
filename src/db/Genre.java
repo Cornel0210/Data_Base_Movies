8e307be93e3298619cf3e45960a5cff4f305f170
Cornel <140949562+Cornel0210@users.noreply.github.com>
2023-09-01 12:21:09 +0300

    add Genre class

---
+++ src/db/Genre.java
@@ -0,0 +1,31 @@
+package db;
+
+public class Genre {
+    private int _id;
+    private String genre;
+    private int movieId;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+}
